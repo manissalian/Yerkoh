998207a9f09bddae485073e60e4cd713f736c93d
Raffi <rmanisalian@cinemoz.com>
2019-06-06 13:12:32 +0300

    updated port code

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const app = express()
-const port = 3000
+//const port = 3000
 
 const search = require('./api/search.js')
 const convert = require('./api/convert.js')
 const download = require('./api/download.js')
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+let port = process.env.PORT
+if (port == null || port == "") {
+  port = 3000
+}
+app.listen(port)
+
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}`)
+// })
 
 app.get('/search/youtube', search.youtube)
 app.get('/convert/youtubeToMp3', convert.youtubeToMp3)
